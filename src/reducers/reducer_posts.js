4e302897bc1fb768abbbd3357f85e08d07fc0521
Eamon Scullion <escull638@gmail.com>
2018-11-12 17:26:17 +0000

    Implement a blog using react router and redux form

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,15 @@
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
+
+export default function(state = {}, action) {
+  switch (action.type) {
+    case DELETE_POST:
+      // creates a new state object without the ID 
+      return _.omit(state, action.payload);
+    case FETCH_POSTS:
+      return _.mapKeys(action.payload.data, 'id');
+    case FETCH_POST:      
+      return { ...state, [action.payload.data.id]: action.payload.data };
+    default:
+      return state;
+  }
+}
